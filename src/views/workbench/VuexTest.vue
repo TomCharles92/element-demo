742b4a665dd1f79d589ae8ceb44ca8b060e61477
Tommy <zhatao1992@163.com>
2019-04-12 19:37:53 +0800

    ES6 class

--- src/views/workbench/VuexTest.vue
+++ src/views/workbench/VuexTest.vue
@@ -1,13 +1,26 @@
 <template>
-  <h1>VuexTest</h1>
+  <div>
+    <el-button @click="test()">
+      Test
+    </el-button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "VuexTest"
+import Mortgage from "../../commonClass.js";
+
+export default {
+  name: "VuexTest",
+  methods: {
+    test() {
+      let a = new Mortgage(200000, 30, 5);
+      let b = new Mortgage(100000, 30, 5);
+      console.log(a);
+      console.log(b, Object.keys(b));
     }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
