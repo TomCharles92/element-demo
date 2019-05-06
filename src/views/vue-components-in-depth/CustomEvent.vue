e3931458dd8da62777416518a6f4ab89e327b7f0
Tommy <zhatao1992@163.com>
2019-05-07 00:23:27 +0800

    自定义事件

--- src/views/vue-components-in-depth/CustomEvent.vue
+++ src/views/vue-components-in-depth/CustomEvent.vue
@@ -2,23 +2,39 @@
   <div>
     <h1>自定义组件</h1>
 
+    <h3>自定义组件的 v-model</h3>
     <base-checkbox v-model="lovingVue"></base-checkbox>
     <CustomInput :value="product" @input="product = $event"></CustomInput>
     <CustomInput v-model="product"></CustomInput>
     {{ product }}
+    <h3>.sync修饰符</h3>
+    <button @click="isShow = true">显示子组件</button>
+    <TextDocument v-show="isShow" :isShow="isShow" @update:isShow="isShow = $event"></TextDocument>
+    <br>
+    <button @click="isShow2 = true">显示子组件</button>
+    <TextDocumentSync v-show="isShow2" :isShow.sync="isShow2"></TextDocumentSync>
   </div>
 </template>
 
 <script>
 import BaseCheckbox from "./childComponent/BaseCheckbox";
 import CustomInput from "./childComponent/CustomInput";
+import TextDocument from "./childComponent/TextDocument";
+import TextDocumentSync from "./childComponent/TextDocumentSync";
 export default {
   name: "CustomEvent",
-  components: { BaseCheckbox, CustomInput },
+  components: { BaseCheckbox, CustomInput, TextDocument, TextDocumentSync },
   data: () => ({
     lovingVue: true,
-    product: "产品"
-  })
+    product: "产品",
+    doc: {
+      title: "标题"
+    },
+    isShow: false,
+    isShow2: false
+  }),
+  methods: {
+  }
 };
 </script>
 
---
+++ src/views/vue-components-in-depth/childComponent/TextDocumentSync.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    TextDocument
+    <p><button @click="$emit('update:isShow', false)">子组件隐藏按钮</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextDocumentSync",
+  props: ["isShow"]
+}
+</script>
+
+<style scoped>
+div {
+  background: lightblue;
+}
+</style>
+
