3cea8faf0a2331ca1a1fddc5a618cf934a71bd89
TomCharles92 <b8%a37@UPQKl>
2020-06-11 20:12:07 +0800

    富文本编辑

--- src/views/plugins/RichText/index.vue
+++ src/views/plugins/RichText/index.vue
@@ -3,23 +3,13 @@
     <h1>RichText</h1>
     <editor
       v-model="content"
-      api-key="no-api-key"
-      :init="{
-         height: 500,
-         menubar: false,
-         plugins: [
-           'advlist autolink lists link image charmap print preview anchor',
-           'searchreplace visualblocks code fullscreen',
-           'insertdatetime media table paste code help wordcount'
-         ],
-         toolbar:
-           'undo redo | formatselect | bold italic backcolor | \
-           alignleft aligncenter alignright alignjustify | \
-           bullist numlist outdent indent | removeformat | help'
-       }"
+      api-key="a00hkgvtiginxy0do9pjwfxhsb9tpcmfymq7tuzqhn1hfeha"
+      :init="initConfiguration"
     />
 
-    <el-button @click="submit">提交</el-button>
+    <el-button @click="submit" type="primary" style="display: block; margin: 1em auto;">提交</el-button>
+
+    <div v-html="content"></div>
   </div>
 </template>
 
@@ -33,13 +23,37 @@ export default {
   },
   data() {
     return {
-      content: ""
-    }
+      content: "",
+      plugins: [
+        "advlist anchor autolink autosave code codesample colorpicker colorpicker contextmenu directionality emoticons fullscreen hr image imagetools insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print save searchreplace spellchecker tabfocus table template textcolor textpattern visualblocks visualchars wordcount"
+      ],
+      toolbar: [
+        "searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample",
+        "hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fullscreen"
+      ],
+      initConfiguration: {
+        height: 500,
+        plugins: plugins, // 插件
+        toolbar: toolbar, // 工具栏
+        language: "zh_CN",
+        menubar: "file edit insert view format table", // 顶部菜单栏
+
+        end_container_on_empty_block: true,
+        powerpaste_word_import: "clean",
+        code_dialog_height: 450,
+        code_dialog_width: 1000,
+        advlist_bullet_styles: "square",
+        advlist_number_styles: "default",
+        // imagetools_cors_hosts: ["www.tinymce.com", "codepen.io"],
+        default_link_target: "_blank",
+        link_title: false
+        // nonbreaking_force_tab: true
+      }
+    };
   },
   methods: {
     submit() {
       console.log(this.content, typeof this.content);
-      
     }
   }
 };
